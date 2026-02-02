8ef2df0263f4d22d8849fa985b4ea59797d3fff9
Mazzega_Dev <=>
2026-02-02 13:19:18 -0300

    feat: category, user, controller, repository, service

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -5,6 +5,43 @@ import { UserInputDto, UserUpdateDto } from "../interfaces/userDTO";
 export default class UserRepository {
    readonly prisma = Prisma;
 
+   async createUser(data: UserInputDto): Promise<User> {
+      return await this.prisma.user.create({
+         data: {
+            user_email: data.user_email,
+            user_name: data.user_name,
+            user_pass: data.user_pass,
+         },
+      });
+   }
+
+   async listUsers(): Promise<User[]> {
+      return await this.prisma.user.findMany();
+   }
+
+   async findById(user_id: number): Promise<User | null> {
+      return await this.prisma.user.findUnique({
+         where: { user_id: user_id },
+      });
+   }
+
+   async updateUser(data: UserUpdateDto): Promise<User> {
+      return await this.prisma.user.update({
+         where: { user_id: data.user_id },
+         data: {
+            user_email: data.user_email,
+            user_name: data.user_name,
+            user_pass: data.user_pass,
+         },
+      });
+   }
+
+   async deleteUser(user_id: number): Promise<User> {
+      return await this.prisma.user.delete({
+         where: { user_id: user_id },
+      });
+   }
+
    async validateUser(
       user_email: string,
       user_pass: string,
