dfb2f7fb2d1d9462fb4c2399e5dae4f58221dec2
Mazzega_Dev <=>
2026-02-04 15:37:07 -0300

    feat: categoryRepository

---
+++ src/repositories/categoryRepository.ts
@@ -0,0 +1,48 @@
+import { Category } from "../generated/prisma/client";
+import { Prisma } from "../database/database";
+import { CategoryInputDto, CategoryUpdateDto } from "../interfaces/categoryDTO";
+
+export default class CategoryRepository {
+   readonly prisma = Prisma;
+
+   async createCategory(data: CategoryInputDto): Promise<Category> {
+      return await this.prisma.category.create({
+         data: {
+            cate_name: data.cate_name,
+            user: {
+               connect: { user_id: data.user_id },
+            },
+         },
+      });
+   }
+
+   async listCategory(user_id: number): Promise<Category[]> {
+      return await this.prisma.category.findMany({
+         where: { user_id: user_id },
+      });
+   }
+
+   async listById(cate_id: number): Promise<Category | null> {
+      return await this.prisma.category.findUnique({
+         where: { cate_id: cate_id },
+      });
+   }
+
+   async updateCategory(data: CategoryUpdateDto): Promise<Category> {
+      return await this.prisma.category.update({
+         where: { cate_id: data.cate_id },
+         data: {
+            cate_name: data.cate_name,
+            user: {
+               connect: { user_id: data.user_id },
+            },
+         },
+      });
+   }
+
+   async deleteCategory(cate_id: number): Promise<Category> {
+      return await this.prisma.category.delete({
+         where: { cate_id: cate_id },
+      });
+   }
+}
