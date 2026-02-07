a1c459dff38a50011a50d2161b851b490a9b1e4f
Mazzega_Dev <=>
2026-02-06 21:27:54 -0300

    formating

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,143 +1,125 @@
 import { Request, Response } from "express";
-import {
-   UserEmailParams,
-   UserInputDto,
-   UserParams,
-   UserUpdateDto,
-} from "../interfaces/userDTO";
+import { UserEmailParams, UserInputDto, UserParams, UserUpdateDto } from "../interfaces/userDTO";
 import { User } from "../generated/prisma/client";
 import UserService from "../services/userService";
 
 export default class UserController {
-   private readonly userServ: UserService;
+	private readonly userServ: UserService;
 
-   constructor() {
-      this.userServ = new UserService();
-   }
+	constructor() {
+		this.userServ = new UserService();
+	}
 
-   async createUser(req: Request, res: Response): Promise<Response> {
-      try {
-         const { user_email, user_name, user_pass } = req.body as UserInputDto;
+	async createUser(req: Request, res: Response): Promise<Response> {
+		try {
+			const { user_email, user_name, user_pass } = req.body as UserInputDto;
 
-         const obj: UserInputDto = {
-            user_email,
-            user_name,
-            user_pass,
-         };
+			const obj: UserInputDto = {
+				user_email,
+				user_name,
+				user_pass,
+			};
 
-         const created: User = await this.userServ.createUser(obj);
+			const created: User = await this.userServ.createUser(obj);
 
-         return res
-            .status(201)
-            .json({ msg: "Novo usuario cadastrado!", data: created });
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(201).json({ msg: "Novo usuario cadastrado!", data: created });
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 
-   async listUser(req: Request, res: Response): Promise<Response> {
-      try {
-         const list: User[] = await this.userServ.listUser();
+	async listUser(req: Request, res: Response): Promise<Response> {
+		try {
+			const list: User[] = await this.userServ.listUser();
 
-         return res.status(200).json(list);
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(200).json(list);
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 
-   async findByid(req: Request<UserParams>, res: Response): Promise<Response> {
-      try {
-         const { user_id } = req.params as UserParams;
+	async findByid(req: Request<UserParams>, res: Response): Promise<Response> {
+		try {
+			const { user_id } = req.params as UserParams;
 
-         const user: User = await this.userServ.findById(user_id);
+			const user: User = await this.userServ.findById(user_id);
 
-         return res.status(200).json(user);
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(200).json(user);
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 
-   async findByEmail(
-      req: Request<UserEmailParams>,
-      res: Response,
-   ): Promise<Response> {
-      try {
-         const { user_email } = req.params as UserEmailParams;
+	async findByEmail(req: Request<UserEmailParams>, res: Response): Promise<Response> {
+		try {
+			const { user_email } = req.params as UserEmailParams;
 
-         const user: User = await this.userServ.findById(user_email);
+			const user: User = await this.userServ.findById(user_email);
 
-         return res.status(200).json(user);
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(200).json(user);
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 
-   async updateUser(req: Request, res: Response): Promise<Response> {
-      try {
-         const { user_id, user_email, user_name, user_pass } =
-            req.body as UserUpdateDto;
+	async updateUser(req: Request, res: Response): Promise<Response> {
+		try {
+			const { user_id, user_email, user_name, user_pass } = req.body as UserUpdateDto;
 
-         const obj: UserUpdateDto = {
-            user_id,
-            user_email,
-            user_name,
-            user_pass,
-         };
+			const obj: UserUpdateDto = {
+				user_id,
+				user_email,
+				user_name,
+				user_pass,
+			};
 
-         const created: User = await this.userServ.updateUser(obj);
+			const created: User = await this.userServ.updateUser(obj);
 
-         return res
-            .status(201)
-            .json({ msg: "Dados do usuario alterados!", data: created });
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(201).json({ msg: "Dados do usuario alterados!", data: created });
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 
-   async deleteUser(
-      req: Request<UserParams>,
-      res: Response,
-   ): Promise<Response> {
-      try {
-         const { user_id } = req.params as UserParams;
+	async deleteUser(req: Request<UserParams>, res: Response): Promise<Response> {
+		try {
+			const { user_id } = req.params as UserParams;
 
-         const deleted: User = await this.userServ.deleteUser(user_id);
+			const deleted: User = await this.userServ.deleteUser(user_id);
 
-         return res
-            .status(200)
-            .json({ msg: "Usuario deletado!", data: deleted });
-      } catch (error: any) {
-         console.log(error);
-         if (error.statusCode) {
-            return res.status(error.statusCode).json(error.message);
-         }
+			return res.status(200).json({ msg: "Usuario deletado!", data: deleted });
+		} catch (error: any) {
+			console.log(error);
+			if (error.statusCode) {
+				return res.status(error.statusCode).json(error.message);
+			}
 
-         return res.status(500).json({ msg: "Erro interno" });
-      }
-   }
+			return res.status(500).json({ msg: "Erro interno" });
+		}
+	}
 }
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -3,67 +3,64 @@ import { Prisma } from "../database/database";
 import { UserInputDto, UserUpdateDto } from "../interfaces/userDTO";
 
 export default class UserRepository {
-   private readonly prisma = Prisma;
+	private readonly prisma = Prisma;
 
-   async createUser(data: UserInputDto): Promise<User> {
-      return await this.prisma.user.create({
-         data: {
-            user_email: data.user_email,
-            user_name: data.user_name,
-            user_pass: data.user_pass,
-         },
-      });
-   }
+	async createUser(data: UserInputDto): Promise<User> {
+		return await this.prisma.user.create({
+			data: {
+				user_email: data.user_email,
+				user_name: data.user_name,
+				user_pass: data.user_pass,
+			},
+		});
+	}
 
-   async listUsers(): Promise<User[]> {
-      return await this.prisma.user.findMany();
-   }
+	async listUsers(): Promise<User[]> {
+		return await this.prisma.user.findMany();
+	}
 
-   async findUserById(user_id: number): Promise<User | null> {
-      return await this.prisma.user.findUnique({
-         where: { user_id: user_id },
-      });
-   }
+	async findUserById(user_id: number): Promise<User | null> {
+		return await this.prisma.user.findUnique({
+			where: { user_id: user_id },
+		});
+	}
 
-   async updateUser(data: UserUpdateDto): Promise<User> {
-      return await this.prisma.user.update({
-         where: { user_id: data.user_id },
-         data: {
-            user_email: data.user_email,
-            user_name: data.user_name,
-            user_pass: data.user_pass,
-         },
-      });
-   }
+	async updateUser(data: UserUpdateDto): Promise<User> {
+		return await this.prisma.user.update({
+			where: { user_id: data.user_id },
+			data: {
+				user_email: data.user_email,
+				user_name: data.user_name,
+				user_pass: data.user_pass,
+			},
+		});
+	}
 
-   async deleteUser(user_id: number): Promise<User> {
-      return await this.prisma.user.delete({
-         where: { user_id: user_id },
-      });
-   }
+	async deleteUser(user_id: number): Promise<User> {
+		return await this.prisma.user.delete({
+			where: { user_id: user_id },
+		});
+	}
 
-   async validateUser(
-      user_email: string,
-      user_pass: string,
-   ): Promise<User | null> {
-      const user = await this.prisma.user.findUnique({
-         where: { user_email: user_email },
-      });
+	async validateUser(user_email: string, user_pass: string): Promise<User | null> {
+		const user = await this.prisma.user.findUnique({
+			where: { user_email: user_email },
+		});
 
-      if (!user) {
-         return null;
-      }
+		if (!user) {
+			return null;
+		}
 
-      if (user.user_pass != user_pass) {
-         return null;
-      }
+		if (user.user_pass != user_pass) {
+			return null;
+		}
 
-      return user;
-   }
+		return user;
+	}
 
-   async findUserByEmail(user_email: string): Promise<User | null> {
-      return await this.prisma.user.findUnique({
-         where: { user_email: user_email },
-      });
-   }
+	async findUserByEmail(user_email: string): Promise<User | null> {
+		return await this.prisma.user.findUnique({
+			where: { user_email: user_email },
+		});
+	}
 }
